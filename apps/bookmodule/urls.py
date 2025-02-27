0d50354d1250d58e9561733d0c2b248f475719a0
iisara0 <sarah.aljamhan@gmail.com>
2025-02-28 02:18:57 +0300

    Lab4 solution

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.index),
-    path('index2/<int:val1>/', views.index2),
-    path('<int:bookId>', views.viewbook),
+from . import views  # Import views from the current app
 
+urlpatterns = [
+    path('', views.index, name="books.index"),  # Homepage
+    path('list_books/', views.list_books, name="books.list_books"),  # Books list page
+    path('<int:bookId>/', views.one_book, name="books.one_book"),  # Single book details
+    path('aboutus/', views.about_us, name="books.aboutus"),  # About us page
 ]
